3fe1e04c2743b6bb27bb0fa508013f113359ac42
Jhonatan-2007 <jesguerra@estudiante.uniajc.edu.co>
2025-03-10 18:26:40 +0000

    Asignacion de variables de Sala

---
+++ Cine/Sala.java
@@ -0,0 +1,15 @@
+public calss Sala {
+    int numero;
+    boolean[][] asientos;
+    String tipo;
+
+    public Sala (int numero, String tipo) {
+        this.numero = numero;
+        this.tipo = tipo;
+        if (tipo.equals("35mm")) {
+            asientos = new boolean [8][12];
+        } else {
+            asientos = new boolean [6][12];
+        }
+    }
+}
