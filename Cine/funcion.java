b4141829f2141761156bf6e7a35f6312547720b7
Luis.nfz <lfgonzalezvergara@estudiante.uniajc.edu.co>
2025-03-10 18:30:08 +0000

    Declaracion de variables de la clase funcion.

---
+++ Cine/funcion.java
@@ -0,0 +1,12 @@
+package Cine;
+public class funcion {
+    Sala sala;
+    String horario;
+    Pelicula paelicula;
+    public funcion(Sala sala, String horario, Pelicula pelicula){
+        this.sala = sala;
+        this.horario = horario;
+        this.pelicula = pelicula;
+    }
+
+}
