b11219590b870c5a8fedacf62bbe829c29ec63f3
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-03-10 18:42:44 +0000

    Crear donde se almacenara los componentes de cada clase

--- Cine/CineApp.java
+++ Cine/CineApp.java
@@ -3,7 +3,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CineApp {
-    
+    static ArrayList<Pelicula> peliculas = new ArrayList<Pelicula>();
+    static ArrayList<Sala> salas = new ArrayList<Sala>();   
+    static ArrayList<Funcion> funciones = new ArrayList<Funcion>();
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         
     }
