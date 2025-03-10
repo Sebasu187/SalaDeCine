a02315527eb32e64371b8eacddbe3565c47a8e40
Sebasu187 <sceballosa@estudiante.uniajc.edu.co>
2025-03-10 18:53:28 +0000

    Creamos el menu y pedimos al usuario que ingrese-main

--- Cine/CineApp.java
+++ Cine/CineApp.java
@@ -9,6 +9,25 @@ public class CineApp {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        salas.add(new Sala(1, "35mm"));
+        salas.add(new Sala(2, "35mm"));
+        salas.add(new Sala(3, "3D"));
+        while (true) {
+            System.out.println("/n--- menu principal---");
+            System.out.println("1. Crear película");
+            System.out.println("2. Asignar función");
+            System.out.println("3. Vender entradas");
+            System.out.println("4. Salir");
+
+            System.out.print("Seleccione una opción: ");
+            int opcion = scanner.nextInt();
+            scanner.nextLine(); 
+            
+
+
+
+            
+        }
         
     }
 
